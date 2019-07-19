b5bb25de0bf87bb8c529c7bea7e26d0b7954ad04
p2 <370887876@qq.com>
2019-07-19 18:29:08 +0800

    first

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$url = $_SERVER['HTTP_HOST'];
+if($url != '') {
+	header("location:https://".$url."/index.html");
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Pandoras</title>
+</head>
+</html>
